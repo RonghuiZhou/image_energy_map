627b49a02a622c65c7761fbd0dc369056ad0a5d4
Ronghui Zhou <zhou.uf@gmail.com>
2020-10-13 08:13:46 -0400

    Add files via upload

--- display_energy_map.py
+++ display_energy_map.py
@@ -1,10 +1,10 @@
+# import libraries
 import os
 import numpy as np
 import cv2
 
-
-# define a function to get the energy map
-def get_energy_map(img):
+# define a function to get the energy map usihg L1 norm
+def get_energy_map_L1(img):
     # convert to a grayscale image if it's a color image
     if len(img.shape) == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -15,9 +15,26 @@ def get_energy_map(img):
     gradient_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, borderType=cv2.BORDER_CONSTANT)
 
     # add the absolute values of gradient_x and gradient_y
-    energy_map = np.abs(gradient_x) + np.abs(gradient_y)
+    energy_map_L1 = np.abs(gradient_x) + np.abs(gradient_y)
 
-    return energy_map
+    return energy_map_L1
+
+
+# define a function to get the energy map usihg L2 norm
+def get_energy_map_L2(img):
+    # convert to a grayscale image if it's a color image
+    if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # make the datatype as float64 for calculation
+    img = img.astype(np.float64)
+    # use the sobel filter for edge detection in both x and y directions, add some border
+    gradient_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, borderType=cv2.BORDER_CONSTANT)
+    gradient_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, borderType=cv2.BORDER_CONSTANT)
+
+    # add the absolute values of gradient_x and gradient_y
+    energy_map_L2 = np.sqrt(gradient_x ** 2 + gradient_y ** 2)
+
+    return energy_map_L2
 
 
 def visualize_energy_map(energy_map):
@@ -28,17 +45,21 @@ def visualize_energy_map(energy_map):
 
 def main():
     # read in image
-    source_dir = '.\src'
+    source_dir = '.\source'
     dolphin = os.path.join(source_dir, 'dolphin.png')
     img = cv2.imread(dolphin, 1)
 
     # calculate the energy map
-    energy_map = get_energy_map(img)
-    energy_map_vis = visualize_energy_map(energy_map)
+    energy_map_L1 = get_energy_map_L1(img)
+    energy_map_L2 = get_energy_map_L2(img)
+
+    energy_map_vis_L1 = visualize_energy_map(energy_map_L1)
+    energy_map_vis_L2 = visualize_energy_map(energy_map_L2)
 
     # display the image
     cv2.imshow('image', img.astype(np.uint8))
-    cv2.imshow('energy_map', energy_map_vis)
+    cv2.imshow('energy_map_L1', energy_map_vis_L1)
+    cv2.imshow('energy_map_L2', energy_map_vis_L2)
 
     # if push 'Esc', destroy the display
     k = cv2.waitKey(0)
@@ -50,15 +71,17 @@ def main():
     elif k == ord('s'):
 
         # define the location to save the energy map
-        out_dir = '.\dst'
+        out_dir = '.\output'
 
         # make the directory if not exists
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
-        out_energy_map = os.path.join(out_dir, 'dolphin_energy_map.png')
+        out_energy_map_L1 = os.path.join(out_dir, 'dolphin_energy_map_L1.png')
+        out_energy_map_L2 = os.path.join(out_dir, 'dolphin_energy_map_L2.png')
         # write the energy map and destroy the window
-        cv2.imwrite(out_energy_map, energy_map_vis)
+        cv2.imwrite(out_energy_map_L1, energy_map_vis_L1)
+        cv2.imwrite(out_energy_map_L2, energy_map_vis_L2)
         cv2.destroyAllWindows()
 
 
